084e0bbc89e182f47a76ace4abecf60582c24b1e
Felipe <felipegustavotheboss@gmail.com>
2023-12-13 22:28:59 -0300

    added build

--- main.js
+++ main.js
@@ -45,9 +45,6 @@ function createWindow() {
 
 	state.manage(mainWindow);
 
-	// Open DevTools - Remove for PRODUCTION!
-	mainWindow.webContents.openDevTools();
-
 	// Listen for window being closed
 	mainWindow.on('closed', () => {
 		mainWindow = null;
